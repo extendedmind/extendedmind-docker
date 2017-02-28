7ea1515387ccf9a68faaaaeee48c8cd339131bc4
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-02-28 15:10:34 +0200

    Added possibility add a custom Piwik configuration file that uses enviroment variables to /etc/piwik/templates/piwik.config.ini.php

--- piwik/docker-entrypoint.sh
+++ piwik/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -e /etc/piwik/templates/piwik.config.ini.php ]; then
+  envsubst < /etc/piwik/templates/piwik.config.ini.php > /usr/src/piwik/config/config.ini.php
+fi
+
 if [ ! -e piwik.php ]; then
 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
 	chown -R www-data .
